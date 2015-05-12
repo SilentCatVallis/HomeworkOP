5217f1a7ca2f9fe6bd7d7e647b55882f2691c227
DenisovAlexander <vallis@olympus.ru>
2015-05-12 23:32:23 +0500

    MapMaker

---
+++ MapMaker/MapMaker/MapMakerShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MapMaker
+{
+	[TestFixture]
+	class MapMakerShould
+	{
+		[Test]
+		public void ReturnMap()
+		{
+			const int height = 30;
+			const int width = 30;
+			var mapMaker = new MapMaker(height, width);
+			var map = mapMaker.GetMap();
+			for (var y = 0; y < height + 3; y++)
+			{
+				Console.WriteLine(map[y]);
+			}
+		}
+	}
+}
